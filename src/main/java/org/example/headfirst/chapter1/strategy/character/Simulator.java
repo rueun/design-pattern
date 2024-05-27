2c9f9ebe0258a3f7964b2ad2448145d3dbb3f814
rueun <dkfah1229@naver.com>
2024-05-27 14:39:59 +0900

    fix: character simulator 파일 수정

--- src/main/java/org/example/headfirst/chapter1/strategy/character/Simulator.java
+++ src/main/java/org/example/headfirst/chapter1/strategy/character/Simulator.java
@@ -4,14 +4,13 @@ import org.example.headfirst.chapter1.strategy.character.behavior.AxeBehavior;
 
 public class Simulator {
     public static void main(String[] args) {
-        java.lang.Character queen = new Queen();
-        java.lang.Character troll = new Troll();
-        java.lang.Character knight = new Knight();
+        Character queen = new Queen();
+        Character troll = new Troll();
+        Character knight = new Knight();
 
         queen.fight();
         troll.fight();
         knight.fight();
-
 
         System.out.println("Queen changes weapon to Axe");
         queen.setWeaponBehavior(new AxeBehavior());
