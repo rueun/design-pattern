258e4fb6e7f6def445752d3c9fc73c7ae2c522a6
rueun <dkfah1229@naver.com>
2024-05-27 16:12:43 +0900

    feat: 체감온도를 출력하는 HeatIndexDisplay 추가

--- src/main/java/org/example/headfirst/chapter2/observer/weather/WeatherStation.java
+++ src/main/java/org/example/headfirst/chapter2/observer/weather/WeatherStation.java
@@ -3,6 +3,7 @@ package org.example.headfirst.chapter2.observer.weather;
 import org.example.headfirst.chapter2.observer.weather.data.WeatherData;
 import org.example.headfirst.chapter2.observer.weather.display.CurrentConditionDisplay;
 import org.example.headfirst.chapter2.observer.weather.display.ForecastDisplay;
+import org.example.headfirst.chapter2.observer.weather.display.HeatIndexDisplay;
 import org.example.headfirst.chapter2.observer.weather.display.StatisticsDisplay;
 
 public class WeatherStation {
@@ -12,9 +13,12 @@ public class WeatherStation {
         final CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
         final StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         final ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        final HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
+        System.out.println("--------------------------------------------------");
         weatherData.setMeasurements(82, 70, 29.2f);
+        System.out.println("--------------------------------------------------");
         weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
---
+++ src/main/java/org/example/headfirst/chapter2/observer/weather/display/HeatIndexDisplay.java
@@ -0,0 +1,37 @@
+package org.example.headfirst.chapter2.observer.weather.display;
+
+import org.example.headfirst.chapter2.observer.weather.Observer;
+import org.example.headfirst.chapter2.observer.weather.data.WeatherData;
+
+public class HeatIndexDisplay implements Observer, DisplayElement {
+    private float heatIndex = 0.0f;
+    private final WeatherData weatherData;
+
+    public HeatIndexDisplay(final WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(final float temp, final float humidity, final float pressure) {
+        heatIndex = computeHeatIndex(temp, humidity);
+        display();
+    }
+
+    private float computeHeatIndex(final float t, final float rh) {
+        float index = (float)((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh)
+                + (0.00941695 * (t * t)) + (0.00728898 * (rh * rh))
+                + (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+                (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+                (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) +
+                (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+                0.000000000843296 * (t * t * rh * rh * rh)) -
+                (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+        return index;
+    }
+
+    public void display() {
+        System.out.printf("Heat index is %s%n", heatIndex);
+    }
+
+}
