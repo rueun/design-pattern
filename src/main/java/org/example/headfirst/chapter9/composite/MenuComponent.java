a65d6fa16dd5a02163d50f34053e2e5ecc9ab58d
rueun <dkfah1229@naver.com>
2024-11-05 18:57:46 +0900

    feat: 컴포지트 패턴 - 카페 메뉴에 컴포지트 패턴 적용

---
+++ src/main/java/org/example/headfirst/chapter9/composite/MenuComponent.java
@@ -0,0 +1,38 @@
+package org.example.headfirst.chapter9.composite;
+
+/**
+ * 어떤 메서드는 menu 에서만, 어떤 메서드는 menuItem 에서만 사용되기 때문에 UnsupportedOperationException 을 던진다.
+ */
+public abstract class MenuComponent {
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public MenuComponent getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isVegetarian() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/org/example/headfirst/chapter9/composite/Waitress.java
@@ -0,0 +1,13 @@
+package org.example.headfirst.chapter9.composite;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
